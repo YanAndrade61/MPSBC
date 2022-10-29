b35c61429de004540344c6d1ba9c26f7027130f7
yan <yanccomp@gmail.com>
2022-10-29 00:38:25 -0300

    Resolucao RO.17

---
+++ roteiro0/paprimas.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int char2dec(char c);
+
+int main(){
+
+    string palavra;
+    int n;
+    bool ehPrimo;
+
+    while (true){
+        
+        cin >> palavra;
+        if(!cin)break;
+        
+        n= 0;
+        ehPrimo = true;
+        for(char c: palavra){
+            n += char2dec(c);
+        }
+
+        for (int i = 2; i < n; i++){
+            if(n%i == 0){
+                ehPrimo= false;
+                break;
+            }
+        }
+        if(ehPrimo)
+            cout << "It is a prime word.\n";
+        else
+            cout << "It is not a prime word.\n";
+
+    }
+    
+    
+    return 0;
+}
+
+int char2dec(char c){
+
+    if(c > 'Z')
+        return c - 96;
+    
+    return c - 65 + 27;
+}
