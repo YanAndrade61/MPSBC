509b109321c9ce33fdf1399ee0f06ecc87e69c21
yan <yanccomp@gmail.com>
2022-11-02 18:54:53 -0300

    Resolucao do R.06

--- R1/uri1467.cpp
+++ R1/uri1467.cpp
@@ -5,22 +5,16 @@ using namespace std;
 int main(){
 
     int A,B,C;
-    while(true){
+    while(cin >> A >> B >> C){
         
-        cin >> A >> B >> C;
-        if(!cin)break;
-        if(A == B && A == C){
-           cout << "*\n";
-        } 
-        else if(A==B){
+        if(A == B && A == C)
+           cout << "*\n";   
+        else if(A==B)
             cout << "C\n";
-        }
-        else if(A==C){
+        else if(A==C)
             cout << "B\n";
-        }
-        else if(B==C){
+        else if(B==C)
             cout << "A\n";
-        }
         
     }
     
